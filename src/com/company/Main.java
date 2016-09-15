9c56ecae44f96608fed330f7765cee7191c161d6
adetoito <adetoito2@gmail.com>
2016-09-15 14:30:36 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Math Class
+        double p = Math.abs(5);
+        double exp = Math.pow(2, 3);
+        // exp holds the value of 8 (2 to the power of 3 is 8 / 2 * 2 * 2 is 8)
+        double pie = Math.PI;
+
+        //ceil is short for ceiling
+        //Math.ceil(4.5) -----> 5
+            // Opposite of Integer Truncation
+        //Math.random
+        int rd = (int)(Math.random() * 100);
+        System.out.println(rd);
+
+    }
+}
